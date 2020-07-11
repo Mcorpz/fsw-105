6bf4e06556c9db04ae0c4f44cb45cda1da7b5e15
Mcorpz <apex_coder@yahoo.com>
2020-07-11 11:01:49 -0500

    week2

---
+++ week2/statementolympics.js
@@ -0,0 +1,15 @@
+if(5 > 3) {
+    console.log("is greater than")
+}
+
+if("cat" != 3) {
+    console.log("is the length")
+}
+
+var Brad=21;
+if( Brad > 18) {
+    console.log("Is allowed to go to movie")
+} else {
+    console.log("Is NOT allowed to go to movie")
+}
+
