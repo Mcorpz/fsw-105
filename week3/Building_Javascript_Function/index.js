c7699e4f8ac0cd29f80adbde87913d4b463f3c28
Mcorpz <apex_coder@yahoo.com>
2020-07-18 12:42:50 -0500

    rough

--- week3/Building_Javascript_Function/index.js
+++ week3/Building_Javascript_Function/index.js
@@ -1,3 +1,21 @@
 function sum(num1,num2,){
-    console.log(2 + 2);
+    return Math.log(num1, num2)
 }
+
+console.log(2 + 2);
+
+function highest_of_three(num1, num2, num3)
+{
+    return Math.max(num1, num2, num3);  
+}
+
+console.log(highest_of_three(12,-40,18));
+
+
+function isEven(value) {
+    if (value%2 == 0)
+        return true;
+    else
+        return false;
+}
+console.log(isEven(3)) 
